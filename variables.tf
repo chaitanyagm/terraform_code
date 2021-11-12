cbab65d77aa9b9d570cd14261c6cb271406d9125
chaitanyagm <55421191+chaitanyagm@users.noreply.github.com>
2021-11-12 17:02:31 +1100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+# List of variables required for Azure Cloud Services
+variable "client_id" {
+}
+
+variable "client_secret" {
+}
+
+variable "rg_name" {
+}
+
+variable "location" {
+}
+
+variable "subscription_id" {
+}
+
+variable "tenant_id" {
+}
+
+variable "arraylist" {
+
+}
